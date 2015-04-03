afdb2fd3e6306c4c887f111ac2057b58f9d41a23
Andy Kitson <andrew.kitson@gmail.com>
2015-04-02 22:17:23 -0700

    (fix) Free up root when we're done with the recursion

--- wrapper.js
+++ wrapper.js
@@ -1,35 +1,43 @@
-
-var stackTree = function (args, parent) {
+var StackTree = function (args, parent) {
   this.args = args;
   this.parent = parent;
   this.children = [];
   this.result = undefined; //just for clarity
 };
+StackTree.prototype.add = function (childTree) {
+  this.children.push(childTree);
+}
 
 var visualize = function (fn, name) {
-  var root = new stackTree(); 
-  var currentStack = root; 
+  var root, currentStack;
 
   return function () {
     var args = Array.prototype.slice.call(arguments);
 
-    var newLevel = new stackTree(args, currentStack);
-    currentStack.children.push(newLevel);
+    // first pass through, set up empty root node
+    if (!root) root = currentStack = new StackTree();
+
+    var newLevel = new StackTree(args, currentStack);
+    currentStack.add(newLevel);
     currentStack = newLevel;
 
     //do viz stuff -- must draw before return or we don't see anything until recursion bottoms out
     //just placeholder for now
-    console.log(currentStack);
+    showStack(root);
 
     //evaluate our function!
     var result = currentStack.result = fn.apply(null, args);
     currentStack = currentStack.parent;
 
+    if (currentStack === root) {
+      // we're all done, so allow our stacks to be garbage collected
+      currentStack = root = undefined;
+    }
     return result ;
   };
 };
 
-
+//examples for testing
 var factViz = visualize(function (n) {
   if (n === 0) return 1;
   return n * factViz(n - 1);
